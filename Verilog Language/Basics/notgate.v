92b33808ed15d8d1bb3467e9267f86ac1071eafb
Panda-Code-Master <pes1202101930@pesu.pes.edu>
2024-01-26 15:34:19 +0530

    Finished problem inverter

---
+++ Verilog Language/Basics/notgate.v
@@ -0,0 +1,21 @@
+## Gate Structural modelling
+module top_module( input in, output out );
+    
+    not(out,in);        
+
+endmodule
+
+## Data-Flow modelling
+module top_module( input in, output out );
+    
+    assign out = ~in;       
+
+endmodule
+
+##Behavior modelling
+module top_module( input in, output out );
+    
+    always@(in)
+        out = ~in;        
+
+endmodule
